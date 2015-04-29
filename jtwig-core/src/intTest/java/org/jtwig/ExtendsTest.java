b3ac557a9bce50c2cda240307c39cdd3b98c1c4f
Joao De Melo <jmelo@lyncode.com>
2015-04-29 20:58:40 +0100

    Checking nested extends functionality

--- jtwig-core/src/intTest/java/org/jtwig/ExtendsTest.java
+++ jtwig-core/src/intTest/java/org/jtwig/ExtendsTest.java
@@ -15,4 +15,22 @@ public class ExtendsTest extends AbstractIntegrationTest {
 
         assertThat(result, is("Override"));
     }
+
+    @Test
+    public void nestedExtends() throws Exception {
+        JtwigTemplate template = defaultStringTemplate("{% extends 'classpath:/example/extends/nested-extendable-template.twig' %}{% block one %}Override{% endblock %}");
+
+        String result = template.render(JtwigModel.newModel());
+
+        assertThat(result, is("Override"));
+    }
+
+    @Test
+    public void nestedExtendsWithoutOverride() throws Exception {
+        JtwigTemplate template = defaultStringTemplate("{% extends 'classpath:/example/extends/nested-extendable-template.twig' %}");
+
+        String result = template.render(JtwigModel.newModel());
+
+        assertThat(result, is("One"));
+    }
 }
